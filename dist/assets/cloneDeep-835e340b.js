5dd881983853a20f43a96944768ece9f03f154ce
achng <2867381901@qq.com>
2023-06-14 16:26:25 +0800

    test 2

---
+++ dist/assets/cloneDeep-835e340b.js
@@ -0,0 +1,1 @@
+import{bC as o}from"./index-760fc3c0.js";var r=1,n=4;function a(e){return o(e,r|n)}export{a as c};
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mall/",
   plugins: [
     vue(), 
     vueJsx(),
